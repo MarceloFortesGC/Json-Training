7d2e5a98fdf0bc9d3323ed821eab9df1e372a6d2
Marcelo Fortes <63818742+MarceloFortesGC@users.noreply.github.com>
2020-12-15 14:27:41 -0300

    Home.jsx Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Data from "../data/data.json";
-import Button from "../components/button/Button";
+import Button from "../components/button/Registration_Button";
 import "../styles.css";
 
 export default function Header() {
